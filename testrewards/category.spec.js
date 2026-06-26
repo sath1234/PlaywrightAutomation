a623358d3106456de3826922d28781be9df8f859
sath1234 <sathyakarthikeyan70@gmail.com>
2026-06-26 23:31:57 +0530

    Added testrewards/category.spec.js

--- testrewards/category.spec.js
+++ testrewards/category.spec.js
@@ -3,6 +3,9 @@ const { test, chromium } = require('@playwright/test');
 test('RewardscategoryTest', async () => {
   const browser = await chromium.launch({
     headless: false,
+    executablePath: "C:\\Users\\DELL\\AppData\\Local\\Santa\\Application\\santa.exe"
+    // OR
+    // executablePath: "C:/Users/DELL/AppData/Local/Santa/Application/santa.exe"
   });
 
   const context = await browser.newContext();
@@ -18,17 +21,27 @@ test('RewardscategoryTest', async () => {
 
   const page = await context.newPage();
 
-  await page.goto('https://rewards.santabrowser.com');  
-  const Quests = page.locator("//nav[@class='flex-1 pr-1']//span[text()='Quests']");   
-  const spaces = page.locator("//nav[@class='flex-1 pr-1']//span[text()='Spaces']");  
-  const myrewards = page.locator("//span[text()='My Rewards']");  
-  const rank = page.locator("//nav[@class='flex-1 pr-1']//span[text()='Rank']")
-  await Quests.click();  
-  await spaces.click();  
-  await myrewards.click();   
-  await rank.click(); 
+  await page.goto('https://rewards.santabrowser.com', {
+    waitUntil: 'networkidle',
+  });
+
+  const quests = page.locator("//nav[@class='flex-1 pr-1']//span[text()='Quests']");
+  const spaces = page.locator("//nav[@class='flex-1 pr-1']//span[text()='Spaces']");
+  const myRewards = page.locator("//span[text()='My Rewards']");
+  const rank = page.locator("//nav[@class='flex-1 pr-1']//span[text()='Rank']");
+
+
+  await quests.click();  
+
+
+  await spaces.click();
+
+
+  await myRewards.click();
+
+
+  await rank.click();
+
 
   await browser.close();
-
-
 });
